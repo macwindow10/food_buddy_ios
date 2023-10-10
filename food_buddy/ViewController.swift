df5d8c9cb9bcf7453500085d6772da8d9d08a7a7
MacBook <mac.window.10@gmail.com>
2023-10-10 17:32:03 +0500

    initial

---
+++ food_buddy/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  food_buddy
+//
+//  Created by Mac on 10/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sleep(3)
+    }
+}
+
