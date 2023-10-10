8b5aee545ec891a3490c066af6450b8c0816570b
MacBook <mac.window.10@gmail.com>
2023-10-10 17:50:08 +0500

    splash screen

--- food_buddy/ViewController.swift
+++ food_buddy/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonOrderFood: UIButton!
+    @IBOutlet var buttonManageRestaurant: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         sleep(3)
+        
+        buttonOrderFood.titleLabel?.font =  UIFont(name: "Helvetica", size: 32)
+        buttonManageRestaurant.titleLabel?.font =  UIFont(name: "Helvetica", size: 32)
     }
 }
 
