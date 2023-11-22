1b25e908e10a1e8a90b061f0429882026a267487
Mac <mac@Macs-Mac.local>
2023-11-23 00:48:15 +0500

    login screen

--- food_buddy/LoginViewController.swift
+++ food_buddy/LoginViewController.swift
@@ -9,24 +9,75 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet var buttonOrderFood: UIButton!
-    @IBOutlet var buttonManageRestaurant: UIButton!
+    @IBOutlet var textUsername: UITextField!
+    @IBOutlet var textPassword: UITextField!
+    @IBOutlet var buttonLogin: UIButton!
+    @IBOutlet var buttonSignUp: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        buttonManageRestaurant.isHidden = true
         sleep(3)
-    
     }
     
     @IBAction func buttonClick_OrderFood(_ sender: UIButton) {
+        /*
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let newViewController = storyBoard.instantiateViewController(withIdentifier: "vcRestaurants") as! RestaurantsViewController
+        let newVißewController = storyBoard.instantiateViewController(withIdentifier: "vcRestaurants") as! RestaurantsViewController
         self.navigationController?.pushViewController(newViewController, animated: true)
+         */
+        if (textUsername.text! == "" || textPassword.text! == "") {
+            return
+        }
+        var components = URLComponents(string: Common.BaseURL + "food_buddy_api/api.php")!
+        components.queryItems = [
+            URLQueryItem(name: "action", value: "login"),
+            URLQueryItem(name: "username", value: textUsername.text!)
+            URLQueryItem(name: "password", value: textPassword.text!)
+        ]
+        components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        let request = URLRequest(url: components.url!)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            
+            //print(response!)
+            do {
+                
+                let json = try JSONSerialization.jsonObject(with: data!) as! NSDictionary
+                print(json)
+                if (json.count == 3 && (json["status"] as! String) == "true")
+                {
+                    let users = json["users"] as! NSArray
+                    for i in 0..<users.count {
+                        let item = users[i] as! Dictionary<String, String>
+                        let userId = item["id"] ?? ""
+                        let userUserName = item["username"] ?? ""
+                        let userName = item["name"] ?? ""
+                        let userAddress = item["address"] ?? ""
+                        let userMobile = item["mobile"] ?? ""
+                        let userType = item["user_type"] ?? ""
+                        let defaults = UserDefaults.standard
+                        defaults.set(userId, forKey: UserDefaultKeys.keyUserId)
+                        defaults.set(userUserName, forKey: UserDefaultKeys.keyUserName)
+                        defaults.set(userName, forKey: UserDefaultKeys.keyName)
+                        defaults.set(userAddress, forKey: UserDefaultKeys.keyAddress)
+                        defaults.set(userMobile, forKey: UserDefaultKeys.keyMobile)
+                        defaults.set(userType, forKey: UserDefaultKeys.keyType)
+                    }
+
+                    DispatchQueue.main.async {
+                        
+                    }
+                }
+                
+            } catch {
+                print("error")
+            }
+        });
+        task.resume();
     }
     
-    @IBAction func buttonClick_ManageFood(_ sender: UIButton) {
+    @IBAction func buttonClick_SignUp(_ sender: UIButton) {
     }
 }
 
---
+++ food_buddy/UserDefaultKeys.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultKeys.swift
+//  food_buddy
+//
+//  Created by Mac on 11/23/23.
+//
+
+import Foundation
+
+struct UserDefaultKeys {
+    static let keyUserId = "UserId"
+    static let keyUserName = "UserUserName"
+    static let keyName = "UserName"
+    static let keyAddress = "UserAddress"
+    static let keyMobile = "UserMobile"
+    static let keyType = "UserType"
+}
