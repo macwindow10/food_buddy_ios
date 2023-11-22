bd7e457fc13132ffe18be786b1dcb69bd6fbef7b
Mac <mac@Macs-Mac.local>
2023-11-23 00:22:28 +0500

    login screen

--- food_buddy/LoginViewController.swift
+++ food_buddy/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet var buttonOrderFood: UIButton!
     @IBOutlet var buttonManageRestaurant: UIButton!
