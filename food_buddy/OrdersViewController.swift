30cb7bc377001530ac7de940428ab8b851ae5452
MacBook <mac.window.10@gmail.com>
2023-12-08 00:14:18 +0500

    show No order if there is no order

--- food_buddy/OrdersViewController.swift
+++ food_buddy/OrdersViewController.swift
@@ -21,6 +21,26 @@ class OrdersViewController: UIViewController, UITableViewDelegate, UITableViewDa
         populateOrders()
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        var numOfSections: Int = 0
+        if self.orders.count > 0
+        {
+            tableView.separatorStyle = .singleLine
+            numOfSections = 1
+            tableView.backgroundView = nil
+        }
+        else
+        {
+            let noDataLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+            noDataLabel.text = "No order"
+            noDataLabel.textColor = UIColor.black
+            noDataLabel.textAlignment = .center
+            tableView.backgroundView = noDataLabel
+            tableView.separatorStyle = .none
+        }
+        return numOfSections
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.orders.count;
     }
