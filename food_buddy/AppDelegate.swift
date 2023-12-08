c09abcbca2dd33d3b39284c261645d200e9da7a2
MacBook <mac.window.10@gmail.com>
2023-12-08 06:59:50 +0500

    show No order if there is no order

--- food_buddy/AppDelegate.swift
+++ food_buddy/AppDelegate.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    func registerForPushNotifications() {
+      //1
+        UNUserNotificationCenter.current()
+            .requestAuthorization(
+                options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                    print("Permission granted: \(granted)")
+                    guard granted else { return }
+                    self?.getNotificationSettings()
+        }
+    }
+
+    func getNotificationSettings() {
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+        print("Notification settings: \(settings)")
+      }
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        registerForPushNotifications()
         return true
     }
 
